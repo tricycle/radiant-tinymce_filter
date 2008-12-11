36d2f31d0d97fbcc19812d873b6eda82325969f1
Andrew Buntine <info@andrewbuntine.com>
2008-12-11 12:13:48 +1100

    Updated TinyMCE config so unnecessary buttons are not dislpayed and resizing is enabled

--- public/javascripts/extensions/tiny_mce/tiny_mce_settings.js
+++ public/javascripts/extensions/tiny_mce/tiny_mce_settings.js
@@ -1,13 +1,22 @@
 tinyMCE.init({   
   editor_selector : "mceEditor",
   editor_deselector : "noMceEditor",
-	mode : "textareas",
-	theme : "advanced",
+  mode : "textareas",
+  theme : "advanced",
+  theme_advanced_buttons1 : "bold,italic,underline,undo,redo,copy,paste,bullist,numlist,link,unlink,image,forecolor,formatselect",
+  theme_advanced_buttons2 : "", 
+  theme_advanced_buttons3 : "",
+  theme_advanced_resizing: true,
+  theme_advanced_resizing_max_width: 1180,
+  theme_advanced_resizing_min_width: 400,
+  theme_advanced_resizing_min_height: 200,
+  theme_advanced_resizing_max_height: 700,
+  theme_advanced_resizing_use_cookie: false,
   extended_valid_elements: "r:*,hr[rel|class|title]",
   plugins: "codeprotect",
   content_css: "/javascripts/extensions/tiny_mce/tiny_mce_content.css",
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_statusbar_location : "bottom",
-	apply_source_formatting : true
+  theme_advanced_toolbar_location : "top",
+  theme_advanced_toolbar_align : "left",
+  theme_advanced_statusbar_location : "bottom",
+  apply_source_formatting : true
 });
