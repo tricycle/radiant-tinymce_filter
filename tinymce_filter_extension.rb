e99b6c4c6989b4871bec585680c171a0e75f4ddd
Gerrit Kaiser <gerrit@gerritkaiser.de>
2009-01-12 20:14:21 +1100

    make dialog helper usable in the admin interface & Refactor extension initializer
    * changed version, attribution and info url
    * extracted activation logic into self-descriptive methods
    * included helper module in the right controller
    * add preliminary link dialog partial to the admin interface

--- tinymce_filter_extension.rb
+++ tinymce_filter_extension.rb
@@ -1,9 +1,9 @@
 require_dependency 'application'
 
 class TinymceFilterExtension < Radiant::Extension
-  version "1.0"
+  version "1.1"
   description "Provides a TinyMCE filter for the Radiant CMS."
-  url "http://code.google.com/p/radiant-tinymce-extension/"
+  url "http://github.com/tricycle/radiant-tinymce_filter/tree"
   
   define_routes do |map|
     map.connect '/admin/pagetree', :controller => 'wysiwyg', :action => 'pagetree'
@@ -11,10 +11,31 @@ class TinymceFilterExtension < Radiant::Extension
   end
   
   def activate
-    # Load the filter
-    TinyMceFilter    
+    preload_filter
     
-    # Add the appropriate stylesheets to the javascripts array in the page and snippet controller
+    pages_controller.helper WysiwygHelper
+    admin.page.edit.add :main, "/wysiwyg/link_dialog", :after => "edit_buttons"
+    
+    include_tinymce_javascripts
+  end
+  
+  def deactivate
+  end
+  
+private
+  def preload_filter
+    TinyMceFilter
+  end
+  
+  def pages_controller
+    defined?(Admin::PagesController) ? Admin::PagesController : Admin::PageController
+  end
+  
+  def snippets_controller
+    defined?(Admin::SnippetsController) ? Admin::SnippetsController : Admin::SnippetController
+  end
+
+  def include_tinymce_javascripts
     include_js = lambda do
       before_filter :add_tinymce_javascripts, :only => [:edit, :new]
       private
@@ -22,15 +43,8 @@ class TinymceFilterExtension < Radiant::Extension
         @javascripts << 'extensions/tiny_mce/tiny_mce' << 'extensions/tiny_mce/tiny_mce_settings' << 'extensions/tiny_mce/tinymce_filter'
       end
     end
-    if defined?(Admin::PagesController) && defined(Admin::PagesController)
-      Admin::PagesController.class_eval &include_js
-      Admin::SnippetsController.class_eval &include_js
-    else
-      Admin::PageController.class_eval &include_js
-      Admin::SnippetController.class_eval &include_js
-    end
-  end
-  
-  def deactivate
+    
+    pages_controller.class_eval(&include_js)
+    snippets_controller.class_eval(&include_js)
   end
 end
