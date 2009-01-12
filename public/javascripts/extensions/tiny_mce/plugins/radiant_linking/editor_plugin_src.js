0baefee5af4a559980e4fa0d4b20bb6bb38b0974
Gerrit Kaiser <gerrit@gerritkaiser.de>
2009-01-12 20:22:26 +1100

    cleaned up tinymce-plugin and integrated it with the dummy dialog
    * removed all unneeded “advanced link” plugin code
    * modified plugin command code to make a click on the link button open the dialog
    * activate our plugin in the settings

--- public/javascripts/extensions/tiny_mce/plugins/radiant_linking/editor_plugin_src.js
+++ public/javascripts/extensions/tiny_mce/plugins/radiant_linking/editor_plugin_src.js
@@ -1,78 +1,53 @@
 /**
- * @author Gerrit Kaiser, Tricycle Developments
- * @copyright Copyright © 2008, Tricycle Developments, All rights reserved.
+ * @author Gerrit Kaiser, Tricycle Developments <gerrit@trike.com.au>
+ * @copyright Copyright Â© 2008, Tricycle Developments, All rights reserved.
  */
 
 (function() {
-	// Load plugin specific language pack
-  // tinymce.PluginManager.requireLangPack('example');
-
 	tinymce.create('tinymce.plugins.RadiantLinkingPlugin', {
-	  /**
-  	 * Returns information about the plugin as a name/value array.
-  	 * The current keys are longname, author, authorurl, infourl and version.
-  	 *
-  	 * @return {Object} Name/value array containing information about the plugin.
-  	 */
-  	getInfo : function() {
-  		return {
-  			longname : 'Radiant Linking Integration',
-  			author : 'Gerrit Kaiser, Tricycle Developments',
-  			authorurl : 'http://tricycledevelopments.com',
-  			infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/example',
-  			version : "1.0"
-  		};
-  	},
-	  
-		/**
-		 * Initializes the plugin, this will be executed after the plugin has been created.
-		 * This call is done before the editor instance has finished it's initialization so use the onInit event
-		 * of the editor instance to intercept that event.
-		 *
-		 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
-		 * @param {string} url Absolute URL to where the plugin is located.
-		 */
-		init : function(ed, url) {
-			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
-			ed.addCommand('mceExample', function() {
-				ed.windowManager.open({
-					file : url + '/dialog.htm',
-					width : 320 + parseInt(ed.getLang('example.delta_width', 0)),
-					height : 120 + parseInt(ed.getLang('example.delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url, // Plugin absolute URL
-					some_custom_arg : 'custom arg' // Custom argument
-				});
-			});
-
-			// Register example button
-			ed.addButton('example', {
-				title : 'example.desc',
-				cmd : 'mceExample',
-				image : url + '/img/example.gif'
-			});
-
-			// Add a node change handler, selects the button in the UI when a image is selected
-			ed.onNodeChange.add(function(ed, cm, n) {
-				cm.setActive('example', n.nodeName == 'IMG');
-			});
+	  getInfo : function() {
+			return {
+				longname : 'Radiant Linking Integration',
+				author : 'Gerrit Kaiser, Tricycle Developments',
+				authorurl : 'http://tricycledevelopments.com',
+				infourl : 'http://github.com/tricycle/radiant-tinymce_filter/tree/',
+				version : '0.1'
+			};
 		},
+		
+		init : function(ed, url) {
+			this.editor = ed;
 
-		/**
-		 * Creates control instances based in the incomming name. This method is normally not
-		 * needed since the addButton method of the tinymce.Editor class is a more easy way of adding buttons
-		 * but you sometimes need to create more complex controls like listboxes, split buttons etc then this
-		 * method can be used to create those.
-		 *
-		 * @param {String} n Name of the control to create.
-		 * @param {tinymce.ControlManager} cm Control manager to use inorder to create new control.
-		 * @return {tinymce.ui.Control} New control instance or null if no control was created.
-		 */
-		createControl : function(n, cm) {
-			return null;
+			// Register commands
+			ed.addCommand('radiantLink', function() {
+				var se = ed.selection;
+
+				// No selection and not in link
+				if (se.isCollapsed() && !ed.dom.getParent(se.getNode(), 'A'))
+					return;
+        
+        // this element comes form the partial wysiwyg/_link_dialog.html.erb
+        // the partial is inserted in tinymce_filter_extension.rb
+        var element = $('tinymce-linking-popup');
+        center(element);
+        element.show();
+			});
+
+			// Register buttons
+			ed.addButton('link', {
+				title : 'advlink.link_desc',
+				cmd : 'radiantLink'
+			});
+
+			ed.addShortcut('ctrl+k', 'advlink.advlink_desc', 'radiantLink');
+
+			ed.onNodeChange.add(function(ed, cm, n, co) {
+				cm.setDisabled('link', co && n.nodeName != 'A');
+				cm.setActive('link', n.nodeName == 'A' && !n.name);
+			});
 		}
 	});
+
 	// Register plugin
 	tinymce.PluginManager.add('radiant_linking', tinymce.plugins.RadiantLinkingPlugin);
 })();
--- public/javascripts/extensions/tiny_mce/tiny_mce_settings.js
+++ public/javascripts/extensions/tiny_mce/tiny_mce_settings.js
@@ -4,10 +4,14 @@ tinyMCE.init({
 	mode : "textareas",
 	theme : "advanced",
   extended_valid_elements: "r:*",
-  plugins: "codeprotect",
+  plugins: "codeprotect,radiant_linking",
   content_css: "/javascripts/extensions/tiny_mce/tiny_mce_content.css",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_statusbar_location : "bottom",
+	// default set of buttons
+  // theme_advanced_buttons1 : "example,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect",
+  // theme_advanced_buttons2 : "bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
+  // theme_advanced_buttons3 : "hr,removeformat,visualaid|sub,sup,charmap",
 	apply_source_formatting : true
 });
