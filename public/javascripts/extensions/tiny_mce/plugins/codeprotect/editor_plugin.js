745c6ed9a5ce8c4c35f3f8c09406ce0818718555
Gerrit Kaiser <gerrit@gerritkaiser.de>
2009-02-20 10:56:54 +1100

    Updated credits/code-comments & removed completely obsolete readme

--- public/javascripts/extensions/tiny_mce/plugins/codeprotect/editor_plugin.js
+++ public/javascripts/extensions/tiny_mce/plugins/codeprotect/editor_plugin.js
@@ -1,28 +1,19 @@
-/* I don't even know where this code comes from. It looks like a
-   simple TinyMCE plugin written for replacing some HTML tags with
-   placeholder images. The original version was modified and copied in
-   the first tinymce_filter extension for Radiant (hosted on Google
-   Code). I updated it to the latest TinyMCE API and fixed some stuff.
-   Daniele Gozzi
-
-   Original copyright notice follows.
+/*
+  
+  * Original Author: Tijmen Schep, Holland, 9-10-2005
+  * modified by “flydesign.nl”, author of the first tinymce_filter extension 
+    for Radiant (http://code.google.com/p/radiant-tinymce-extension/).
+  * onBeforeSetContent Regexp orignally taken from Phil Haack’s blog
+  * updated it to the latest TinyMCE API and fixed by Daniele Gozzi
+  * Matching-regex fixed by Andrew Buntine/Tricycle Developments
+  * Updated to not mangle namespaced Radius-tags by Gerrit Kaiser/Tricycle Developments
+  
 */
-/**** 
- * I'm not even gonna copyright this, that's just silly.
- * Feel free to improve on this code and re-upload it.
- * Tijmen Schep, Holland, 9-10-2005
- ****/
 
 (function() {
   tinymce.create('tinymce.plugins.CodeProtectPlugin', {
     init: function(ed, url) {
         ed.onGetContent.add(function(ed,o) {
-          // Original regex:
-          //   var regex = new RegExp('<hr rel="([^"]*)?" class="mceItemRadiantCode" title="(.*?)" />', 'g');
-
-          // I have modified this regular expression to make the "class" attribute optional because it was being 
-          // stripped from the element when it's rendered in TinyMCE (which means the original regex doesn't match
-          // and the element remains escaped and trapped inside a HR element).
           var regex = new RegExp('<hr rel="([^"]*)?"( class="mceItemRadiantCode")? title="(.*?)" />', 'g');
           var m = o.content.match(regex);
           if (!(m == null)) {
@@ -33,7 +24,6 @@
         }
       });
       ed.onBeforeSetContent.add(function(ed, o) {
-        /* Regexp taken from Phil Haack blog. Thanks Phil! */
         var m = o.content.match(/<\/?r:\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>/g);
         var content = o.content;
         if (!(m == null)) {
